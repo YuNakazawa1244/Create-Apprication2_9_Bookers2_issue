50c2f79ce053ffcc5c721a8e8bc9478644507c08
YuNakazawa1244 <g_o_w_yu321903@yahoo.ne.jp>
2021-03-26 12:26:11 +0000

    [Add]5-9

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,28 @@
 class UsersController < ApplicationController
+  
+  def new
+  end
+  
+  def create
+  end
+  
   def index
   end
+  
+  def show
+  end
+  
+  def destroy
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    # redirect_to user_path(user.id)
+  end
+  
 end
