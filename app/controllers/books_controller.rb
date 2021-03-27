13aa7d641ff5d44cd3526313430c070f9c2253fe
YuNakazawa1244 <g_o_w_yu321903@yahoo.ne.jp>
2021-03-27 10:49:37 +0000

    [Add]5-9

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,29 @@
 class BooksController < ApplicationController
-  def index
-  end
+
+def index
+  @user = current_user
+end
+
+def new
+end
+
+def create
+end
+
+def edit
+end
+
+def update
+end
+
+def destroy
+end
+
+
+private
+
+def user_params
+  params.require(:user).permit(:name,:profile_image,:introduction)
+end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_23_105623) do
+ActiveRecord::Schema.define(version: 2021_03_27_103057) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "profile_images", force: :cascade do |t|
-    t.string "name"
-    t.string "image_id"
-    t.integer "profile_image_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -35,10 +27,10 @@ ActiveRecord::Schema.define(version: 2021_03_23_105623) do
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.string "name"
-    t.text "introduction"
-    t.integer "profile_image_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "introduction"
+    t.string "profile_image_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
