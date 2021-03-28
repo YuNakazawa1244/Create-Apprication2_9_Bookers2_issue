3a26b9d878aacdd38ade91fdd1000814a3190140
YuNakazawa1244 <g_o_w_yu321903@yahoo.ne.jp>
2021-03-28 07:25:25 +0000

    [Add]5-9

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,15 +2,23 @@ class BooksController < ApplicationController
 
 def index
   @user = current_user
+  @book = Book.new
+  @books = Book.all
 end
 
 def new
+  @book = Book.new
 end
 
 def create
+  book = Book.new(book_params)
+  book.user_id = current_user.id
+  book.save
+  redirect_to book_path(book)
 end
 
 def edit
+  @user = User.find(params[:id])
 end
 
 def update
@@ -24,6 +32,10 @@ private
 
 def user_params
   params.require(:user).permit(:name,:profile_image,:introduction)
+end
+
+def book_params
+  params.require(:book).permit(:title,:body,:user_id)
 end
 
 end
