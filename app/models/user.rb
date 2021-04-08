a4e9867da622d0943910542736882f66a089d8c2
YuNakazawa1244 <g.o.w.yu32190331153@gmail.com>
2021-04-08 12:00:47 +0000

    [Add]follow

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,9 +9,27 @@ class User < ApplicationRecord
   has_many :post_comments,dependent: :destroy
 
 # 以下フォローフォロワー機能
-　has_many :xxx,class_name:"Hogeモデル名",foreign_key:"follower_id",dependent: :destroy
+  has_many :follower, class_name: "Relationship",foreign_key: "follower_id", dependent: :destroy
+  has_many :followed, class_name: "Relationship",foreign_key: "followed_id", dependent: :destroy
 
-　has_many :yyy,through: :xxx,source: :zzz
+  has_many :following, through: :follower, source: :followed #自分がフォローしている人
+  has_many :followers, through: :followed, source: :follower #自分をフォローしている人
+
+  #ユーザーをフォローする
+  def follow(user_id)
+    follower.create(followed_id: user_id)
+  end
+
+  #ユーザーのフォローを外す
+  def unfollow(user_id)
+    follower.find_by(followed_id: user_id).destroy
+  end
+
+  #フォローしていればtrueを返す
+  def following?(user)
+    following.include?(user)
+  end
+
 # ここまで
 
   attachment :profile_image
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,16 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
+
   resources :books do
     resource :favorites,only:[:create,:destroy]
     resources :post_comments,only:[:create,:destroy]
   end
-  resources :users, only:[:create,:index,:show,:destroy,:edit]
+
+  resources :users, only:[:create,:index,:show,:destroy,:edit] do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
 
 end
